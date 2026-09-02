9d3f0889328e3bb581f0e6e3867c845ad7aa452f
axjp <63340001+Alpha-404@users.noreply.github.com>
2026-09-02 15:58:15 +0100

    controllable fan check

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
+#include <QCoreApplication>
 #include <iostream>
 #include <dlfcn.h>
+#include <unistd.h>
 
+using Fn = unsigned int (*)();
 using Constructor = void (*)(void*);
 using GetRotationSpeed = int (*)(void*, unsigned short*);
 using SetRotation = int (*)(void*);
 
-int main()
+int main(int argc, char** argv)
 {
+    if (geteuid() != 0) {
+        std::cout << "Root permissions are required as this program works with the EC." << std::endl;
+        exit(1);
+    }
+
+    QCoreApplication app(argc, argv); //to resolve fan_database.sqlite as module_fan expects a qt environment
+
     void* lib = dlopen("./module_fan.so", RTLD_NOW);
 
     if (!lib) {
         std::cerr << dlerror() << '\n';
         return 1;
     }
+
+    auto isControllable =
+        reinterpret_cast<Fn>(
+            dlsym(lib, "_ZN19FanComponentManager27IsFanSpeedControllableModelEv")
+        );
 
     auto ctor = reinterpret_cast<Constructor>(
         dlsym(lib, "_ZN32EmbeddedControllerComponentLinuxC1Ev")
@@ -26,6 +41,12 @@ int main()
         dlsym(lib, "_ZN27EmbeddedControllerComponent15SetFullRotationEv")
     );
 
+    if (!isControllable) {
+        std::cerr << "dlsym: " << dlerror() << '\n';
+        return 1;
+    }
+
+
     if (!ctor || !getRPM || !full) {
         std::cerr << "dlsym failed: " << dlerror() << '\n';
         return 1;
@@ -38,6 +59,7 @@ int main()
     unsigned short rpm = 0;
     int result = getRPM(object, &rpm);
 
+    std::cout << "Fan controllable: " << isControllable() << '\n';
     std::cout << "GetRotationSpeed result: " << result << '\n';
     std::cout << "RPM: " << rpm << '\n';
 
